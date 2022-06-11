49ea6478877db722002fa0ea5752f267fc42c282
Shiy <siqgustavo2@hotmail.com>
2022-06-11 19:10:43 -0300

    fix the date

--- userAuth/urls.py
+++ userAuth/urls.py
@@ -3,10 +3,5 @@ from .views import CreateUser, UserRetrieveUpdate, listUserId, DestroyUser, Upda
 
 urlpatterns = [
     path('createUser', CreateUser.as_view()),
-    path('users', ListUser.as_view()),
-    path('updateUser/<int:pk>', UpdateUser.as_view()),
-    path('deleteUser/<int:pk>', DestroyUser.as_view()),
-    path('listUserId/<int:pk>', listUserId.as_view()),
-    path('userRetrieveUpdate/<int:pk>', UserRetrieveUpdate.as_view()),
-    path('GetAuthenticatedUser', GetAuthenticatedUser.as_view())
+
 ]
